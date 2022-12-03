e8f73e048b8deda9e54282ee330dbe526aafdfd0
singmnj <manujsingh96@gmail.com>
2022-12-03 12:39:14 -0500

    refac: add class to start server

--- runserver.py
+++ runserver.py
@@ -5,10 +5,19 @@ This script runs the price_matching_system application using a development serve
 from os import environ
 from price_matching_system import create_app
 
+class RunServer:
+
+    def __init__(self, HOST, PORT):
+        self.HOST = HOST
+        self.PORT = PORT
+
+    def start_server(self):
+        create_app().run(self.HOST, self.PORT)
+
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    create_app().run(HOST, PORT)
+    RunServer(HOST, PORT).start_server()
